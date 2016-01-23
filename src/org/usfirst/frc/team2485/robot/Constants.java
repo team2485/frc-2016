a2919708dc948cc361831eb502499b1f80e48f0d
Ben Clark <rclark2017@francisparker.org>
2016-01-23 15:08:34 -0800

    Constants preliminary work

--- src/org/usfirst/frc/team2485/robot/Constants.java
+++ src/org/usfirst/frc/team2485/robot/Constants.java
@@ -1,5 +1,17 @@
 package org.usfirst.frc.team2485.robot;
 
-public class Constants {
+import java.io.File;
 
+public class Constants {
+	
+	
+	
+	public String getFileLocation() {
+		return "~/constants.txt";
+	}
+	
+	public File getFile() {
+		String fileLocation = getFileLocation();
+		return new File(fileLocation);
+	}
 }
