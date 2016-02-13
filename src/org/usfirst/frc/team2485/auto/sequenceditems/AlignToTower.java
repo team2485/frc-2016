4894cc2fae6ec89470e2b56542b8872e8decb128
mmaunu <mmaunu@francisparker.org>
2016-02-12 17:58:04 -0800

    Changed some logic, made angleTarget an absolute angle instead of relative

--- src/org/usfirst/frc/team2485/auto/sequenceditems/AlignToTower.java
+++ src/org/usfirst/frc/team2485/auto/sequenceditems/AlignToTower.java
@@ -18,32 +18,40 @@ public class AlignToTower implements SequencedItem {
 
 	private int timeout = 5;
 	private boolean finished = false;
+	private boolean firstTime = true;
 
+	//the angle, according to the AHRS, that we should be pointing towards
+	//to be on-target
 	private double angleTarget;
 
 	private Timer refreshTimer;
 
 	public AlignToTower() {
-		angleTarget = Hardware.ahrs.getYaw();
+		
 	}
 
 	@Override
 	public void run() {
 
+		if(firstTime) {
+			angleTarget = Hardware.ahrs.getYaw();
+			firstTime = false;
+		}
 		double currentAngle = Hardware.ahrs.getYaw();
 
 		if (Math.abs(angleTarget - currentAngle) <= 0.5) {
 			if (refreshTimer == null) {
+				refreshTimer = new Timer();
 				refreshTimer.schedule(new TimerTask() {
 
 					@Override
 					public void run() {
 						try {
-							angleTarget = GRIPReciever.getAngle();
+							angleTarget = Hardware.ahrs.getYaw() + GRIPReciever.getAngle();
 							refreshTimer = null;
 
 							if (Math.abs(angleTarget) <= 0.5) {
-								GRIPReciever.resetCameraSettings();
+								//GRIPReciever.resetCameraSettings();
 								finished = true;
 							}
 
@@ -54,7 +62,7 @@ public class AlignToTower implements SequencedItem {
 				}, 250);
 			}
 		} else {
-			Hardware.driveTrain.rotateTo(angleTarget + Hardware.ahrs.getYaw());
+			Hardware.driveTrain.rotateTo(angleTarget);
 		}
 	}
 
