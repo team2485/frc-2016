43278b0c53a30e81db242b01198dabfdffd8d13f
Aidan Fay <afay2017@francisparker.org>
2016-02-20 13:08:41 -0800

    Changed Battery to have-a PDP rather than be-a PDP

--- src/org/usfirst/frc/team2485/util/Battery.java
+++ src/org/usfirst/frc/team2485/util/Battery.java
@@ -9,22 +9,47 @@ import edu.wpi.first.wpilibj.PowerDistributionPanel;
  * Wrapper class to log PDP data
  * @author Ben Clark
  */
-public class Battery extends PowerDistributionPanel implements Loggable {
+public class Battery implements Loggable {
 
+	private PowerDistributionPanel pdp;
 	public Battery() {
-		super();
+		pdp = new PowerDistributionPanel();
 	}
+	
+	
 	
 	@Override
 	public Map<String, Object> getLogData() {
 		Map<String, Object> logData = new HashMap<String, Object>();
 		
 		logData.put("Name", "Battery");
-		logData.put("Voltage", getVoltage());
-		logData.put("Temperature", getTemperature());
-		logData.put("Total Current", getTotalCurrent());
-		logData.put("Power", getTotalPower());
+		logData.put("Voltage", pdp.getVoltage());
+		logData.put("Temperature", pdp.getTemperature());
+		logData.put("Total Current", pdp.getTotalCurrent());
+		logData.put("Power", pdp.getTotalPower());
 		
 		return logData;
+	}
+
+
+
+	public double getCurrent(int slot) {
+		return pdp.getCurrent(slot);
+	}
+
+	public double getTemperature() {
+		return pdp.getTemperature();
+	}
+	
+	public double getTotalPower() {
+		return pdp.getTotalPower();
+	}
+	
+	public double getVoltage() {
+		return pdp.getVoltage();
+	}
+
+	public double getTotalCurrent() {
+		return pdp.getTotalCurrent();
 	}	
 }
